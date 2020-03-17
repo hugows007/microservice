996dd3fad22f448632c13f9adff83ec1fd09ec3a
Hugo Bezerra Pimentel <hugobezerrapimentel@gmail.com>
2020-03-17 18:23:08 -0300

    [CHANGE]

---
+++ auth/src/main/java/br/com/alura/microservice/auth/config/AuthorizationServerConfigurer.java
@@ -0,0 +1,4 @@
+package br.com.alura.microservice.auth.config;
+
+public class AuthorizationServerConfigurer {
+}
---
+++ auth/src/main/java/br/com/alura/microservice/auth/config/WebSecurityConfigurer.java
@@ -0,0 +1,4 @@
+package br.com.alura.microservice.auth;
+
+public class WebSecurityConfigurer {
+}
